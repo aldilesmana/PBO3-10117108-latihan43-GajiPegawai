49fa9f3a3f8dc3832bcc84f5e7a901d525186e25
aldilesmana <aldilesmana181@gmail.com>
2018-10-22 22:37:32 +0700

    first commit

---
+++ src/main.java
@@ -0,0 +1,16 @@
+//NAMA  :Aldi Lesmana
+//NIM   :10117108   
+//KELAS :IF-3
+
+
+/**
+ *
+ * @author Aldi Lesmana
+ */
+public class main {
+    public static void main(String[] args) {
+        GajiPegawai objG = new GajiPegawai();
+        objG.tampilData("", "", 0, 0, 0, 0);
+        
+    }
+}
